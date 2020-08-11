d1583ea75f6529833f4a996c962b3f9108c63bfc
Hallef Santos <hallefsantos@gmail.com>
2020-08-10 22:16:25 -0300

    Finished 320

--- src/App.svelte
+++ src/App.svelte
@@ -15,20 +15,20 @@
   </div>
 </header>
 
-<main class="px-4 pb-16">
+<main class="px-4 pb-12">
   <div class="mb-8">
     <div class="border border-gray-200 rounded-lg shadow-sm py-2">
       <img class="mx-auto" src="/img/head-black_front.png" alt="">
     </div>
-    <div class="flex justify-between mt-5">
+    <div class="grid gap-2 grid-cols-3 mt-5">
       <div class="px-3 border-2 border-gray-700 rounded-lg shadow-sm">
         <img class="rounded-lg" src="/img/head-black_front.png" alt="">
       </div>
-      <div class="px-3 mx-4 border border-gray-200 rounded-lg shadow-sm">
-        <img class="rounded-lg" src="/img/head-black_front.png" alt="">
+      <div class="px-3 border border-gray-200 rounded-lg shadow-sm">
+        <img class="rounded-lg" src="/img/head-black_front-side-left.png" alt="">
       </div>
       <div class="px-3 border border-gray-200 rounded-lg shadow-sm">
-        <img class="rounded-lg" src="/img/head-black_front.png" alt="">
+        <img class="rounded-lg" src="/img/powerhead_back.png" alt="">
       </div>
     </div>
   </div>
@@ -46,35 +46,109 @@
       This is made possible by a radical, patented technology and concept which we call "PROFILING".
     </p>
 
-    <div class="mt-16 text-gray-900">
-      <h4 class="text-lg mb-3 font-medium">Form Factor</h4>
+    <div class="mt-12 text-gray-900">
+      <h4 class="text-lg mb-3 font-medium text-gray-900">Form Factor</h4>
       <div class="">
         <div class="border-2 border-gray-700 rounded-lg p-5 px-6 mb-5 shadow-sm">
-          <strong class="text-sm font-semibold">Profiler Head</strong>
-          <p class="text-sm mt-2 leading-normal font-light text-gray-500">Compact amplifier head, perfect for a speaker cabinet or desk.</p>
+          <strong class="text-sm mb-2 font-medium block">Profiler Head</strong>
+          <p class="text-sm leading-normal font-light text-gray-500">Compact amplifier head, perfect for a speaker cabinet or desk.</p>
         </div>
         <div class="border border-gray-200 rounded-lg p-5 px-6 shadow-sm">
-          <strong class="text-sm font-semibold">Profiler Rack</strong>
-          <p class="text-sm mt-2 leading-normal font-light text-gray-500">3U rackmount version of the classic profiling amplifier.</p>
+          <strong class="text-sm mb-2 font-medium block">Profiler Rack</strong>
+          <p class="text-sm leading-normal font-light text-gray-500">3U rackmount version of the classic profiling amplifier.</p>
         </div>
       </div>
     </div>
 
-    <div class="mt-16 text-gray-900">
-      <h4 class="text-lg mb-3 font-medium">Power Amp</h4>
+    <div class="mt-12 text-gray-900">
+      <h4 class="text-lg mb-3 font-medium text-gray-900">Power Amp</h4>
       <div class="">
         <div class="border-2 border-gray-700 rounded-lg p-5 px-6 mb-5 shadow-sm">
-          <strong class="text-sm font-semibold">None</strong>
-          <p class="text-sm mt-2 leading-normal font-light text-gray-500">Use in the studio or with your own power amp.</p>
+          <strong class="text-sm mb-2 font-medium block">None</strong>
+          <p class="text-sm leading-normal font-light text-gray-500">Use in the studio or with your own power amp.</p>
         </div>
         <div class="border border-gray-200 rounded-lg p-5 px-6 shadow-sm">
-          <strong class="text-sm font-semibold">Powered</strong>
-          <div>
-            <p class="text-sm mt-2 leading-normal font-light text-gray-500">3U rackmount version of the classic profiling amplifier.</p>
+          <strong class="text-sm mb-2 font-medium block">Powered</strong>
+          <div class="flex justify-between">
+            <p class="flex-1 text-sm leading-normal font-light text-gray-500">3U rackmount version of the classic profiling amplifier.</p>
+            <span class="text-sm font-light ml-3">+ $449</span>
           </div>
         </div>
       </div>
     </div>
 
+    <div class="mt-12 text-gray-900">
+      <h4 class="text-lg mb-3 font-medium text-gray-900">Foot Controller</h4>
+      <div class="">
+        <div class="border-2 border-gray-700 rounded-lg p-5 px-6 mb-5 shadow-sm">
+          <div class="flex justify-between">
+            <p class="flex-1 text-sm font-medium block">None</p>
+            <span class="text-sm font-light ml-3">+ $449</span>
+          </div>
+        </div>
+        <div class="border border-gray-200 rounded-lg p-5 px-6 shadow-sm">
+          <strong class="text-sm font-medium block">Profiler Remote Foot Controller</strong>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="mt-12 text-gray-900">
+    <h4 class="text-lg font-medium border-b border-gray-200 pb-2 text-gray-900">Specifications</h4>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Amp Models</span>
+      <span>200</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Effects Loop</span>
+      <span>Yes</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Inputs</span>
+      <span>2 x 1/4”</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Outputs</span>
+      <span>1 x 1/4”, 2 x XLR, 2 x 1/4”</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>MIDI I/O</span>
+      <span>In/Out/Thru</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Height</span>
+      <span>8.54”</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Width</span>
+      <span>14.88”</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Depth</span>
+      <span>6.81”</span>
+    </div>
+
+    <div class="flex justify-between py-5 border-b border-gray-200">
+      <span>Weight</span>
+      <span>11.73 lbs</span>
+    </div>
   </div>
 </main>
+
+<footer class="py-5 px-4 bg-gray-50 border-t border-gray-200">
+  <div class="flex items-center justify-between">
+    <div class="flex-1 text-right pr-5">
+      <span class="text-gray-900 text-4xl font-bold">$2,717</span>
+    </div>
+    <a href="#" class="inline-block text-white bg-gray-900 px-5 py-3 rounded-lg">Buy now</a>
+  </div>
+</footer>
